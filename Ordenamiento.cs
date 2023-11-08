860b9d8d59fb97f58a92e3f96a3798e575dbcc1a
null <null>
2023-11-08 11:41:38 -0600

    Agregar archivos de proyecto.

---
+++ Ordenamiento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenamientoBusqueda_G2_2024_1
+{
+    internal class Ordenamiento
+    {
+        internal static void Burbuja(int [] A)
+        {
+            int N = A.Length;
+            for (int i = 0; i < N; i++)
+            {
+
+            }
+        }
+    }
+}
